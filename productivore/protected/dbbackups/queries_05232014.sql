75845e4053b61a01b8763dd21b8a8c96cb9fa7e4
crentagon <kiefer.yap@gmail.com>
2014-06-02 15:32:16 +0800

    Changed the color of the title bar. I like this title bar more.

--- productivore/protected/dbbackups/queries_05232014.sql
+++ productivore/protected/dbbackups/queries_05232014.sql
@@ -24,3 +24,23 @@ ADD CONSTRAINT `setting_fields_appling_id_fk`
   REFERENCES `productivore_db`.`applings` (`appling_id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
+
+/*
+	Inserting a new settingmap? Insert into settings, too.
+*/
+
+BEGIN
+	--New appling must be accessible to the users
+	INSERT INTO user_appling_maps (user_id, appling_id)
+	SELECT user_id, NEW.appling_id FROM users;
+END
+
+BEGIN
+	--Insert a new setting_value_map?
+	--New setting map must have values in settings
+	INSERT INTO settings (user_appling_map_id, setting_field_setting_value_map_id, setting_field_id)
+	SELECT user_appling_map_id, NEW.setting_field_setting_value_map_id, NEW.setting_field_id
+	FROM user_applings
+	WHERE appling_id = NEW.appling_id
+	AND is_default = 1;
+END
