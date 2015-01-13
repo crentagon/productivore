c3f040a20831f070c474539939a955de6a2d661f
crentagon <kiefer.yap@gmail.com>
2015-01-13 12:54:55 +0800

    Finished phpdoc for both controllers.

--- protected/controllers/ProtagonalController.php
+++ protected/controllers/ProtagonalController.php
@@ -1,22 +1,44 @@
 <?php
 
+/**
+ * ProtagonalController is a controller class for Productivore's Protagonal appling.
+ *
+ * ProtagonalController is a class which contains the Achievements, Whoops and
+ * Thought Bubble sub-applings.
+ *
+ * @author 	Kiefer Yap <kiefer.yap@gmail.com> 
+ *
+ */
+
 class ProtagonalController extends Controller
 {	
-	
-	public function __construct(){
+	/**
+	* The constructor, which sets the applingId of the controller.
+	* In this case, 4.
+	*/
+	public function __construct() {
 		$this->applingId = 4;
 		parent::__construct();
 	}
 	
-	public function actionIndex(){
-		// echo 'here'; die();
+	/**
+	 * The default 'index' action that is invoked
+	 * when an action is not explicitly requested.
+	 */	
+	public function actionIndex() {
 		$this->setupPage('Protagonal - Productivore', array(
 			'Protagonal' => BASE_URL.'/protagonal'
 		));
 		$this->render('index');
 	}
 	
-	public function actionAchievements($mode = 'index'){
+	/**
+	 * The Achievements action.
+	 *
+	 * This method allows users to view/add/delete their achievments.
+	 * @param string the tab to load: index or complete
+	 */
+	public function actionAchievements($mode = 'index') {
 		$this->setupPage('Protagonal - Productivore', array(
 			'Protagonal' => BASE_URL.'/protagonal',
 			'Achievements' => BASE_URL.'/protagonal/achievements'
@@ -44,7 +66,12 @@ class ProtagonalController extends Controller
 		$this->render('achievements', compact('mode', 'model', 'achievements'));
 	}
 	
-	public function actionThoughts(){
+	/**
+	 * The Thoughts action.
+	 *
+	 * This method allows users to view/add/delete their thought bubbles.
+	 */
+	public function actionThoughts() {
 		$this->loadScripts('protagonal-thoughts.js');
 		$this->loadStyles('protagonal-thoughts.css');
 	
@@ -72,47 +99,78 @@ class ProtagonalController extends Controller
 		$this->render('thoughts', compact('model', 'thoughtList', 'thoughtBubbleList'));
 	}
 	
-	public function actionWhoops(){
+	/**
+	 * The Whoops action.
+	 *
+	 * This method allows users to view/add/delete their, erm, regrets.
+	 */
+	public function actionWhoops() {
 		echo 'whoops';
 		die();
 	}
 	
-	public function actionReflection(){
-		echo 'reflection';
-		die();
-	}
-	
-	public function actionSettings(){
+	/**
+	 * The Settings action.
+	 *
+	 * This method allows users to change settings like date of birth.
+	 */
+	public function actionSettings() {
 		echo 'protagonalSettings';
 		die();
 	}
 	
-	//http://localhost/productivore/protagonal/getNextThoughtBubblesAjax/startingThoughtBubbleId/9/listId/1/
-	public function actionGetNextThoughtBubblesAjax($startingThoughtBubbleId, $listId){
+	/**
+	 * The get next thought bubbles action.
+	 *
+	 * This method is invoked via AJAX when the
+	 * user wants to see their next thought bubbles
+	 * upon scrolling down to the end.
+	 * The parameters are passed using POST.
+	 * To access this function,
+	 * /productivore/productivore/protagonal/getNextThoughtBubblesAjax
+	 *
+	 * @param integer the starting thought bubble id to be loaded
+	 * @param integer the id of the list to be loaded
+	 *
+	 */
+	public function actionGetNextThoughtBubblesAjax($startingThoughtBubbleId, $listId) {
 		$userId = Yii::app()->user->getId();
 		$protagonalHelper = new ProtagonalHelper;
 		$thoughtBubbleList = $protagonalHelper->get_thoughtbubbles_byUserIdListIdThoughtBubbleId($userId, $listId, $startingThoughtBubbleId);
 		echo json_encode($thoughtBubbleList);
-		// $this->debugPrint($thoughtBubbleList);
 	}
 	
-	//http://localhost/productivore/protagonal/getNextThoughtBubblesByListIdAjax/listId/1/
-	public function actionGetNextThoughtBubblesByListIdAjax($listId){
+	/**
+	 * TO-DO: Merge this function with the one above
+	 * http://localhost/productivore/protagonal/getNextThoughtBubblesByListIdAjax/listId/1/
+	 */
+	public function actionGetNextThoughtBubblesByListIdAjax($listId) {
 		$userId = Yii::app()->user->getId();
 		$protagonalHelper = new ProtagonalHelper;
 		$thoughtBubbleList = $protagonalHelper->get_thoughtbubbles_byUserIdListIdThoughtBubbleId($userId, $listId);
 		$protagonalHelper->setActiveThoughtList($userId, $listId);
 		echo json_encode($thoughtBubbleList);
-		// $this->debugPrint($thoughtBubbleList);
 	}
 	
-	//http://localhost/productivore/productivore/protagonal/editachievementfieldajax/field/achievement_name/achievementId/7/value/asdfgh
-	public function actionEditAchievementFieldAjax($field, $achievementId, $value = null){
+	/**
+	 * The edit achievement field action.
+	 *
+	 * This method is invoked via AJAX when the
+	 * user wants to edit a field in their achievements.
+	 * The parameters are passed using POST.
+	 * To access this function,
+	 * /productivore/productivore/protagonal/editachievementfieldajax
+	 *
+	 * @param string the field to be updated: achievement_name,
+	 * achievement_condition, achivement_rewards, completion_notes
+	 * @param integer the id of the achievement to be edited
+	 * @param the new value of the field to be updated
+	 *
+	 */
+	public function actionEditAchievementFieldAjax($field, $achievementId, $value = null) {
 		$requiredFields = array('achievement_condition', 'achievement_rewards');
 	
 		if(in_array($field, $requiredFields)){
-			// echo json_encode(array('error'=>'The field '.$value.' must not be left blank.'));
-			// exit;
 			if($value == null || $value == ''){
 				echo json_encode(array('error'=>'The field '.$field.' must not be left blank.'));
 				exit;
@@ -125,18 +183,39 @@ class ProtagonalController extends Controller
 			$achievements->save();
 		}
 		
-		echo json_encode(array('success'=>'The field '.$field.' has successfully been updated.'));
-		
+		echo json_encode(array('success'=>'The field '.$field.' has successfully been updated.'));	
 	}
 	
-	//http://localhost/productivore/productivore/protagonal/deleteachievementajax/achievementId/8
-	public function actionDeleteAchievementAjax($achievementId){
+	/**
+	 * The delete achievement action.
+	 *
+	 * This method is invoked via AJAX when the
+	 * user wants to delete an achievement.
+	 * The parameters are passed using POST.
+	 * To access this function,
+	 * /productivore/productivore/protagonal/deleteachievementajax
+	 *
+	 * @param integer the id of the achievement to be deleted
+	 *
+	 */
+	public function actionDeleteAchievementAjax($achievementId) {
 		if($achievementId != null){
 			$achievements = Achievements::model()->deleteByPk($achievementId);
 		}
 	}
 	
-	//http://localhost/productivore/productivore/protagonal/markascompleteajax/achievementId/9
+	/**
+	 * The mark achievement as complete action.
+	 *
+	 * This method is invoked via AJAX when the
+	 * user wants to mark an achievement as complete
+	 * The parameters are passed using POST.
+	 * To access this function,
+	 * /productivore/productivore/protagonal/markascompleteajax
+	 *
+	 * @param integer the id of the achievement to be marked.
+	 *
+	 */
 	public function actionMarkAsCompleteAjax($achievementId){
 		if($achievementId != null){
 			$achievements = Achievements::model()->findByPk($achievementId);
@@ -145,28 +224,4 @@ class ProtagonalController extends Controller
 		}
 	}
 	
-	//http://localhost/productivore/productivore/protagonal/updateachievementsettings/valueId/7
-	public function actionUpdateAchievementSettings($valueId = null){
-		if(!Yii::app()->user->isGuest){
-			if($valueId == null){
-				Yii::app()->user->setFlash('error','Something\'s definitely not right here. You\'re not trying to hack the system, are you?');		
-				$this->render('preview');
-			}
-			$fieldId = 3;
-			$update = array($fieldId=>$valueId); //setting_field_id, field_value_map_id
-			$userId = Yii::app()->user->getId(); 
-			
-			$userApplings = new SidebarHelper;
-			$userApplings->update_settingValues_byUserId($userId, $this->applingId, $update);
-			Yii::app()->end();
-		}
-		else{
-			throw new CHttpException(404,'The page could not be found.');
-			$this->render('index');
-		}
-	}
-	
-	
-	
-
 }
