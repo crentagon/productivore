decfc94f6328b8e2c51bce7944075adf73f4d78d
crentagon <kiefer.yap@gmail.com>
2015-06-17 18:02:37 +0800

    Updated gitignore

--- protected/dbbackups/queries_10122014.sql
+++
@@ -1,5 +0,0 @@
-UPDATE applings SET appling_name = 'Protagonal' WHERE appling_id = 4;
-UPDATE applings SET appling_url = 'protagonal' WHERE appling_id = 4;
-UPDATE menus SET menu_name = 'Giga Hype', menu_url = 'gigahype' WHERE menu_id = 4;
-UPDATE menus SET menu_name = 'Whoops', menu_url = 'whoops' WHERE menu_id = 5;
-UPDATE menus SET menu_name = 'Reflection', menu_url = 'reflection' WHERE menu_id = 6;
